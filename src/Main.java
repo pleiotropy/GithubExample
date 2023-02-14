9ff53bf5bfd2defd7ecaf06110baa004d2c4f710
pleiotropy <angela.m.joseph+github@gmail.com>
2023-02-14 15:31:31 -0500

    begin U1L4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Github example");
-        System.out.println("Change 1");
+        // Create new scanner object
+        Scanner s = new Scanner(System.in);
+
+        // Prompt user for their name
+        System.out.print("Please enter your name: ");
+
+        // Get input and store in a variable
+        String name = s.nextLine();
+
+        // Prompt user for their age
+        System.out.print("Please enter your age: ");
+
+        // Get input and store in a variable as an integer
+        int x = s.nextInt();
     }
 }
