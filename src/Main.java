43dcb3cb4f2145bf2d1bfcb606b972c45dc701ba
pleiotropy <angela.m.joseph+github@gmail.com>
2023-02-15 21:50:39 -0500

    U1L6 Compound Assignment Operators

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Create new scanner object
         Scanner s = new Scanner(System.in);
 
-        // Prompt user for their name
-        System.out.print("Please enter your name: ");
+        // Get the user's name
+        System.out.print("Please enter a five-digit number (ex: 25497): ");
+        int userNumber = s.nextInt();
 
-        // Get input and store in a variable
-        String name = s.nextLine();
+        // Get the first digit
+        int first = userNumber / 10000;
+        first++;
+        first %= 10;
 
-        // Prompt user for their age
-        System.out.print("Please enter your age: ");
+        // Get the second digit
+        int second = (userNumber % 10000) / 1000;
+        second++;
+        second %= 10;
 
-        // Get input and store in a variable as an integer
-        int x = s.nextInt();
+        // Get the third digit
+        int third = (userNumber % 1000) / 100;
+        third++;
+        third %= 10;
+
+        // Get the fourth digit
+        int fourth = (userNumber % 100) / 10;
+        fourth++;
+        fourth %= 10;
+
+        // Get the fifth digit
+        int fifth = userNumber % 10;
+        fifth++;
+        fifth %= 10;
+
+        // Print name and average
+        System.out.print("Your number (" + userNumber + ") with each digit incremented by 1 is " + first + second + third + fourth + fifth + ".");
     }
 }
