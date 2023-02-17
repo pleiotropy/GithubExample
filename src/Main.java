adff7f11aa64c2c5253b508400d956b8a91b729d
pleiotropy <angela.m.joseph+github@gmail.com>
2023-02-16 21:38:48 -0500

    U1L7 Type Casting

--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,68 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // Get dollar amount from the user
         Scanner s = new Scanner(System.in);
+        System.out.print("Please enter a dollar amount: ");
+        double dollar = s.nextDouble();
 
-        // Get the user's name
-        System.out.print("Please enter a five-digit number (ex: 25497): ");
-        int userNumber = s.nextInt();
+        // Divide the dollar amount by the value of a quarter to
+        // determine how many quarters you will need
+        int numberOfQuarters = (int) (dollar / 0.25);
 
-        // Get the first digit
-        int first = userNumber / 10000;
-        first++;
-        first %= 10;
+        // Subtract the amount covered by the quarters from the
+        // original dollar amount to get the remaining amount
+        // you still need to cover with other coins
+        double amountAfterQuarters = dollar - ((double)numberOfQuarters * 0.25);
 
-        // Get the second digit
-        int second = (userNumber % 10000) / 1000;
-        second++;
-        second %= 10;
+        // Divide the amountAfterQuarters by the value of a dime
+        // to determine how many dimes you will need
+        int numberOfDimes = (int) (amountAfterQuarters / 0.1);
 
-        // Get the third digit
-        int third = (userNumber % 1000) / 100;
-        third++;
-        third %= 10;
+        // Subtract the amount covered by the dimes from the
+        // amountAfterQuarters to get the remaining amount
+        double amountAfterDimes = amountAfterQuarters - ((double)numberOfDimes * 0.1);
 
-        // Get the fourth digit
-        int fourth = (userNumber % 100) / 10;
-        fourth++;
-        fourth %= 10;
+        // Divide the amountAfterDimes by the value of a nickel
+        // to determine how many nickels you will need
+        int numberOfNickels = (int) (amountAfterDimes / 0.05);
 
-        // Get the fifth digit
-        int fifth = userNumber % 10;
-        fifth++;
-        fifth %= 10;
+        // Subtract the amount covered by the nickels from the
+        // amountAfterDimes to get the remaining amount
+        double amountAfterNickels = amountAfterDimes - ((double)numberOfNickels * 0.05);
 
-        // Print name and average
-        System.out.print("Your number (" + userNumber + ") with each digit incremented by 1 is " + first + second + third + fourth + fifth + ".");
+        // Divide the amountAfterNickels by the value of a penny
+        // to determine how many pennies you will need
+        int numberOfPennies = (int) (amountAfterNickels / 0.01);
+
+        // Add up the number of each coin to get the minimum
+        // number of coins
+        int minimum = numberOfQuarters + numberOfDimes + numberOfNickels + numberOfPennies;
+
+        // Print it out
+        System.out.println("The minimum number of coins is " + minimum + ":");
+        System.out.println(numberOfQuarters + " quarters");
+        System.out.println(numberOfDimes + " dimes");
+        System.out.println(numberOfNickels + " nickels");
+        System.out.println(numberOfPennies + " pennies");
+
+        /*
+        double a = 18.24;  // 18
+        double b = 212.5;  // 213
+        double c = -5.3;   // -5
+        double d = -25.77; // -26
+
+        double newA = a - (int) a;
+        double newB = b - (int) b;
+        double newC = c - (int) c;
+        double newD = d - (int) d;
+
+        System.out.println(newA);
+        System.out.println(newB);
+        System.out.println(newC);
+        System.out.println(newD);
+
+         */
+
     }
 }
